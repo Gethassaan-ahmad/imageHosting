df536b248403d4f27d0fe5d06a325f7dee5cb0a8
hassaan <hassaanahmad001@gmail.com>
2021-12-10 16:54:34 +0500

    Commit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->integer('age');
-            $table->string('image');
+            $table->string('image')->nullable();
             $table->string('token');
             $table->rememberToken();
             $table->timestamps();
